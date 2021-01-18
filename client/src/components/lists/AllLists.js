167b8591a6cf178c1549a5ff9bab2049e684eb43
cbeattie <courtney.j.beattie@gmail.com>
2021-01-18 16:17:29 +0800

    Commenting out some stuff in AllLists

--- client/src/components/lists/AllLists.js
+++ client/src/components/lists/AllLists.js
@@ -15,26 +15,25 @@ import List from './List'
 // }
 
 const AllLists = () => {
-    const [lists, setLists] = useState([]);
+    // const [lists, setLists] = useState([]);
 
-    useEffect(() => {
-        axios.get('/list').then((response) => {
-            setLists(response.data);
-            console.log("response.data", response.data)
-        })
-    }, [lists])
+    // useEffect(() => {
+    //     axios.get('/list').then((response) => {
+    //         setLists(response.data);
+    //     })
+    // }, [lists])
 
-    const updateDelete = (id) => () => {
-        setLists(lists.filter(x => x._id !== id))
-    }
+    // const updateDelete = (id) => () => {
+    //     setLists(lists.filter(x => x._id !== id))
+    // }
 
     return (
         <>
             <h1>This is the homepage with all the lists</h1>
-            {lists.map((list) => (
+            {/* {lists.map((list) => (
                 <h1>List</h1>
                 // <List key={list._id} category={list.category} />
-            ))}
+            ))} */}
         </>
     )
 }
