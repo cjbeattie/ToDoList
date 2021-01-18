b124e1ea042d1b0c589914d25514591c66e45bb7
cbeattie <courtney.j.beattie@gmail.com>
2021-01-18 17:11:48 +0800

    Editing AddTask

--- client/src/components/lists/AllLists.js
+++ client/src/components/lists/AllLists.js
@@ -2,38 +2,37 @@ import axios from "axios"
 import { useEffect, useState } from 'react';
 import List from './List'
 
-// const AllLists = () => {
-
-//     return (
-//         <>
-//             <h1>This is the homepage with all the lists</h1>
-//             <List />
-//             <List />
-//             <List />
-//         </>
-//     )
-// }
 
 const AllLists = () => {
-    // const [lists, setLists] = useState([]);
+    const [lists, setLists] = useState([]);
+    const [categories, setCategories] = useState([]);
 
-    // useEffect(() => {
-    //     axios.get('/list').then((response) => {
-    //         setLists(response.data);
-    //     })
-    // }, [lists])
+    useEffect(() => {
+        axios.get('/list').then((response) => {
+            setLists(response.data);
+            console.log("lists response.data", response.data);
+        })
 
-    // const updateDelete = (id) => () => {
-    //     setLists(lists.filter(x => x._id !== id))
-    // }
+        axios.get('/category').then((response) => {
+            setCategories(response.data);
+            console.log("categories response.data", response.data);
+        })
+    }, [])
+
+    const updateDelete = (id) => () => {
+        setLists(lists.filter(x => x._id !== id))
+    }
 
     return (
         <>
             <h1>This is the homepage with all the lists</h1>
-            {/* {lists.map((list) => (
-                <h1>List</h1>
-                // <List key={list._id} category={list.category} />
-            ))} */}
+            {lists.map((list) => (
+                <List
+                    key={list._id}
+                    id={list._id}
+                // categoryName={categories.filter(x => x._id === list.category).name}
+                />
+            ))}
         </>
     )
 }
