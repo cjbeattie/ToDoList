74879d58c07470498e2d56673a252a4087d0c5c5
cbeattie <courtney.j.beattie@gmail.com>
2021-01-21 17:26:39 +0800

    Styling signup form roughly

--- client/src/components/users/SignUp.js
+++ client/src/components/users/SignUp.js
@@ -51,43 +51,49 @@ const SignUp = (e) => {
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-      <Card style={{ width: '18rem', height: '15rem' }} >
-        <h1> SignUp Form</h1>
-        <form onSubmit={handleSubmit}>
-          Username:
+      <Card style={{ width: '18rem' }} className="text-left">
+        <Card.Header as="h5">Sign Up</Card.Header>
+        {/* <h1> SignUp Form</h1> */}
+        <Card.Body variant="flush" classname="mb-3">
+
+
+          <form onSubmit={handleSubmit}>
+            Username:
         <input
-            type="text"
-            value={formData.username}
-            onChange={(e) =>
-              setFormData((state) => ({
-                ...state,
-                username: e.target.value,
-              }))
-            }
-          />
-          <br />
+              type="text"
+              value={formData.username}
+              onChange={(e) =>
+                setFormData((state) => ({
+                  ...state,
+                  username: e.target.value,
+                }))
+              }
+            />
+            <br />
         Password:
         <input
-            type="password"
-            value={formData.password}
-            onChange={(e) =>
-              setFormData((state) => ({
-                ...state,
-                password: e.target.value,
-              }))
-            }
-          />
-          <br />
+              type="password"
+              value={formData.password}
+              onChange={(e) =>
+                setFormData((state) => ({
+                  ...state,
+                  password: e.target.value,
+                }))
+              }
+            />
+            <br />
         isAdmin:
         <input
-            type="checkbox"
-            name="isAdmin"
-            value={formData.isAdmin}
-            onChange={handleCheckbox}
-          />
-          <br />
-          <input type="submit" />
-        </form>
+              type="checkbox"
+              name="isAdmin"
+              value={formData.isAdmin}
+              onChange={handleCheckbox}
+            />
+            <br />
+            <br />
+            <input type="submit" />
+          </form>
+        </Card.Body>
       </Card>
     </div>
   );
