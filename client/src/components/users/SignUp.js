f64d7b2efdbd957468ad6f78d6c5ed46007323ec
cjbeattie <courtney.j.beattie+1@gmail.com>
2021-03-15 13:12:14 +0800

    More tidy up of sign in component

--- client/src/components/users/SignUp.js
+++ client/src/components/users/SignUp.js
@@ -17,17 +17,18 @@ const SignUp = (e) => {
       .catch((error) => {
         console.log("error", error);
       });
-    // console.log("event", e);
-    console.log("sign up happened********************")
   };
-
-
 
   const [formData, setFormData] = useState({
     username: "",
     password: "",
     isAdmin: false,
   });
+  const [created, setCreated] = useState(false);
+
+  if (created) {
+    return <Redirect to="/login" />;
+  }
 
   const handleCheckbox = (e) => {
     let target = e.target;
@@ -44,12 +45,6 @@ const SignUp = (e) => {
       isAdmin: value,
     }))
   };
-
-  const [created, setCreated] = useState(false);
-
-  if (created) {
-    return <Redirect to="/login" />;
-  }
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
@@ -99,54 +94,12 @@ const SignUp = (e) => {
             <Button variant="outline-secondary" type="submit">
               Submit
             </Button>
-
+            <br />
+            <br />
             <Form.Text className="text-muted">
               Already have an account? Please <Link to={`/login`} activeClassName="active">Login</Link>
             </Form.Text>
           </Form>
-
-
-
-          {/* <form onSubmit={handleSubmit}>
-            Username:
-        <input
-              type="text"
-              value={formData.username}
-              onChange={(e) =>
-                setFormData((state) => ({
-                  ...state,
-                  username: e.target.value,
-                }))
-              }
-            />
-            <br />
-        Password:
-        <input
-              type="password"
-              value={formData.password}
-              onChange={(e) =>
-                setFormData((state) => ({
-                  ...state,
-                  password: e.target.value,
-                }))
-              }
-            />
-            <br />
-            <br />
-        Admin user:
-        <input
-              type="checkbox"
-              name="isAdmin"
-              value={formData.isAdmin}
-              onChange={handleCheckbox}
-              style={{ width: 20 }}
-            />
-            <br />
-            <br />
-            <input type="submit" />
-            <Link to={`/login`} activeClassName="active">Login</Link>
-            <Button type="submit" variant="outline-secondary">Signup2</Button>
-          </form> */}
         </Card.Body>
       </Card>
     </div>
