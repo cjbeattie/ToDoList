498e5f2a08701f9ae0edaa6b727fbccbaabf5c43
cjbeattie <courtney.j.beattie+1@gmail.com>
2021-03-15 13:05:43 +0800

    Tidying up signup form

--- client/src/components/users/SignUp.js
+++ client/src/components/users/SignUp.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
-import { Card } from 'react-bootstrap'
+import { Card, Button, Form } from 'react-bootstrap'
 import { Link } from "react-router-dom"
 
 const SignUp = (e) => {
@@ -18,6 +18,7 @@ const SignUp = (e) => {
         console.log("error", error);
       });
     // console.log("event", e);
+    console.log("sign up happened********************")
   };
 
 
@@ -57,8 +58,56 @@ const SignUp = (e) => {
         {/* <h1> SignUp Form</h1> */}
         <Card.Body variant="flush" classname="mb-3">
 
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Username"
+                value={formData.username}
+                onChange={(e) =>
+                  setFormData((state) => ({
+                    ...state,
+                    username: e.target.value,
+                  }))
+                } />
+            </Form.Group>
 
-          <form onSubmit={handleSubmit}>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Enter Password"
+                value={formData.password}
+                onChange={(e) =>
+                  setFormData((state) => ({
+                    ...state,
+                    password: e.target.value,
+                  }))
+                } />
+            </Form.Group>
+            <Form.Group controlId="formBasicCheckbox">
+              <Form.Check
+                type="checkbox"
+                label="Admin User"
+                value={formData.isAdmin}
+                onChange={handleCheckbox} />
+              <Form.Text className="text-muted">
+                Admin users can edit categories and see a list of all users. For now, anyone can be an admin user.
+              </Form.Text>
+            </Form.Group>
+            <Button variant="outline-secondary" type="submit">
+              Submit
+            </Button>
+
+            <Form.Text className="text-muted">
+              Already have an account? Please <Link to={`/login`} activeClassName="active">Login</Link>
+            </Form.Text>
+          </Form>
+
+
+
+          {/* <form onSubmit={handleSubmit}>
             Username:
         <input
               type="text"
@@ -83,17 +132,21 @@ const SignUp = (e) => {
               }
             />
             <br />
-        isAdmin:
+            <br />
+        Admin user:
         <input
               type="checkbox"
               name="isAdmin"
               value={formData.isAdmin}
               onChange={handleCheckbox}
+              style={{ width: 20 }}
             />
+            <br />
             <br />
             <input type="submit" />
             <Link to={`/login`} activeClassName="active">Login</Link>
-          </form>
+            <Button type="submit" variant="outline-secondary">Signup2</Button>
+          </form> */}
         </Card.Body>
       </Card>
     </div>
